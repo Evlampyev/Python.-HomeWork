dcd09d1b78a0013c819d88962158184aaa75b77c
Bad_S <Evlampyev.Alex@gmail.com>
2022-12-21 00:00:55 +0500

    Task 2 ended

--- III - seminar (20.12.22)/Task 2.py
+++ III - seminar (20.12.22)/Task 2.py
@@ -2,3 +2,13 @@
 # Пример:
 # - [2, 3, 4, 5, 6] => [12, 15, 16];
 # - [2, 3, 5, 6] => [12, 15]
+
+import random
+
+length = int(input("Введите размер списка "))
+lst = [random.randint(1, 10) for i in range(length)]
+print(lst)
+rez = []
+for i in range(0, length // 2 + length % 2):
+    rez.append(lst[i] * lst[length - i - 1])
+print(rez)
