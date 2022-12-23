9cfe17f0680984f3c20176816b756c5c693e5c81
Bad_S <Evlampyev.Alex@gmail.com>
2022-12-24 00:30:13 +0500

    Task 2 end

---
+++ IV - seminar (23.12.22)/Task 2.py
@@ -0,0 +1,17 @@
+﻿# Задайте натуральное число N. Напишите программу, которая составит список простых множителей числа N.
+
+def SimpleMultiplicities(n):
+    i = 2
+    lst = []
+    while i <= n + 1:
+        if n % i == 0:
+            n = n // i
+            lst.append(i)
+        else:
+            i += 1
+    return lst
+
+
+N = int(input())
+myLst = SimpleMultiplicities(N)
+print(*myLst, sep="*")
