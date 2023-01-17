74b2c35283b237884c65ddb0b25ec60aeced72a9
Bad_S <Evlampyev.Alex@gmail.com>
2023-01-18 00:19:55 +0500

    Homework-VI ended

---
+++ VI - seminar (17.01.23)/Task 2.py
@@ -0,0 +1,9 @@
+﻿# Дан список, вывести отдельно буквы и цифры, пользуясь filter.
+# [12,'sadf',5643] ---> ['sadf'] ,[12,5643]
+
+lst = [12, 'sadf', 5643]
+digit = list(filter(lambda x: type(x) == int, lst))
+str = list(filter(lambda x: not type(x) == int, lst))
+
+print(f"Было: {lst}")
+print(f"Стало: {str}, {digit}")
---
+++ VI - seminar (17.01.23)/Task 3.py
@@ -0,0 +1,9 @@
+﻿# Напишите программу, которая принимает на вход вещественное число и показывает сумму его цифр.
+# Пример:
+# - 6782 -> 23
+# - 0,56 -> 11
+
+number = input('Введите любое число: ')
+newnumber = list(filter(lambda x: x.isdigit(), number))
+newnumber = list(map(int, newnumber))
+print(sum(newnumber))
