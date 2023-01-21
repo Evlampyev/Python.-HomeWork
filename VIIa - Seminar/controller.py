7f349d3362fad41db3ced253af5cf676a38e30e0
Bad_S <Evlampyev.Alex@gmail.com>
2023-01-21 22:17:03 +0500

    Task_hard begin

---
+++ VIIa - Seminar/controller.py
@@ -0,0 +1,24 @@
+﻿import interface
+import model
+import logger
+import view
+
+
+def project_launch():
+    s = interface.select_action()
+    while s != 0:
+        # interface.html_creator()
+        if s == 1:
+            view.data_print()
+        else:
+            add_entry()
+        s = interface.select_action()
+
+
+
+def add_entry():
+    data_list = interface.input_data()
+    data_str = model.unification(data_list)
+    logger.log_data(data_str)
+    logger.log_data_list(data_list)
+    return
